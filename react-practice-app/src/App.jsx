cf4cb8940902403dafaceba4e98410ed596ecca3
daoistcoder <deservekindness@gmail.com>
2023-06-12 17:06:18 +0800

    completed useReducer()

--- react-practice-app/src/App.jsx
+++ react-practice-app/src/App.jsx
@@ -5,8 +5,8 @@ export const CounterComp = () => {
   const [counter, setCounter] = React.useState(0);
   const increment = () => {
     setCounter(counter + 1);
-    console.log(counter)
-  }
+    console.log(counter);
+  };
   //Old World
   // let counter = 1;
   // const increment = () => {
@@ -20,28 +20,70 @@ export const CounterComp = () => {
       <button onClick={increment}>Increment</button>
     </div>
   );
-}
+};
 
 export const InputBoxComp = () => {
-  const [inputText, setInputText] = React.useState('Jerome');
+  const [inputText, setInputText] = React.useState("Jerome");
   let onChange = (event) => {
     const newText = event.target.value;
     setInputText(newText);
     console.log(inputText);
-  }
+  };
   return (
-    <div><input type="text" placeholder="type here..." onChange={onChange} />{inputText}</div>
-  )
-}
+    <div>
+      <input type="text" placeholder="type here..." onChange={onChange} />
+      {inputText}
+    </div>
+  );
+};
 
-
-
+export const ReducerComp = () => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "Increment":
+        return { count: state.count + 1, showText: state.showText };
+      case "toggleShowText":
+        return { count: state.count, showText: !state.showText };
+      default:
+        return state;
+    }
+  };
+  const [state, dispatch] = React.useReducer(reducer, {
+    count: 0,
+    showText: true,
+  });
+  return (
+    <div>
+      <h1>{state.count}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "Increment" });
+          dispatch({ type: "toggleShowText" });
+        }}
+      >
+        Click Here
+      </button>
+      {state.showText && <p>This is a text</p>}
+    </div>
+  );
+};
 
 export default function App() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", fontWeight: "bold", gap: "10px" }}>
-      <CounterComp />
-      <InputBoxComp />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        fontWeight: "bold",
+        gap: "10px",
+        textAlign: "center",
+      }}
+    >
+      {/* <CounterComp />
+      <InputBoxComp /> */}
+      <ReducerComp />
     </div>
-  )
+  );
 }
