f493e4ed8cf558f3442c1611bdc713b0af33890e
daoistcoder <deservekindness@gmail.com>
2023-06-12 16:46:11 +0800

    completed useState

--- react-practice-app/src/App.jsx
+++ react-practice-app/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const CounterComp = () => {
+  //New World w. React.useState()
+  const [counter, setCounter] = React.useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+    console.log(counter)
+  }
+  //Old World
+  // let counter = 1;
+  // const increment = () => {
+  //   counter = counter + 1;
+  //   console.log(counter)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {counter}
+
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export const InputBoxComp = () => {
+  const [inputText, setInputText] = React.useState('Jerome');
+  let onChange = (event) => {
+    const newText = event.target.value;
+    setInputText(newText);
+    console.log(inputText);
+  }
+  return (
+    <div><input type="text" placeholder="type here..." onChange={onChange} />{inputText}</div>
   )
 }
 
-export default App
+
+
+
+export default function App() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", fontWeight: "bold", gap: "10px" }}>
+      <CounterComp />
+      <InputBoxComp />
+    </div>
+  )
+}
